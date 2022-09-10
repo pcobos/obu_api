af4e148d63135695bb59ffab5b6d61a86c914d75
Pedro Cobos <pedro.cobos@live.com>
2022-09-10 15:44:41 -0300

    add test seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Course.create(name: "test", instructor: "Danilo Kim", description: "Test description")
