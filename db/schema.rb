077ab44a0db4e9a5dd68327e47c55bc2fe62ee39
Pedro Cobos <pedro.cobos@live.com>
2022-09-10 12:30:28 -0300

    feat(backend): add Lesson model & controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_10_145452) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_10_153000) do
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.string "instructor"
@@ -19,4 +19,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_10_145452) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "lessons", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "video_link"
+    t.integer "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_lessons_on_course_id"
+  end
+
+  add_foreign_key "lessons", "courses"
 end
