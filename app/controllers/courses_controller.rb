caaf81bce71581a9677ec282ee724a19635c97eb
Pedro Cobos <pedro.cobos@live.com>
2022-09-10 13:50:58 -0300

    feat(back): add nested lessons routes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -24,28 +24,13 @@ class CoursesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /courses/1
-  def update
-    if @course.update(course_params)
-      render json: @course
-    else
-      render json: @course.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /courses/1
-  def destroy
-    @course.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_course
-      @course = Course.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def course_params
-      params.require(:course).permit(:name, :instructor, :description)
-    end
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:name, :instructor, :description)
+  end
 end
